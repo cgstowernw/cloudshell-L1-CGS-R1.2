04165bcad9b466e75213bc15e916c7798fd08ee4
Danny Raveh <danny@cgstowernetworks.onmicrosoft.com>
2017-04-23 15:57:35 +0300

    initial commit

---
+++ tests/test_cgs_driver/test_driver_handler.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+class Testcgs_driverDriverHandler(unittest.TestCase):
+    def setUp(self):
+        super(Testcgs_driverDriverHandler, self).setUp()
+
+    def tearDown(self):
+        super(Testcgs_driverDriverHandler, self).tearDown()
+
+    def test_login(self):
+        pass
+
+    def test_get_resource_description(self):
+        pass
+
+    def test_map_bidi(self):
+        pass
+
+    def test_map_uni(self):
+        pass
+
+    def test_map_clear_to(self):
+        pass
+
+    def test_map_clear(self):
+        pass
+
+    def test_set_speed_manual(self):
+        pass
